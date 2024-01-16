5e95545b107c76d783a4b0c2e3765d8afb584699
Cizen14 <cizenmah@gmail.com>
2024-01-16 19:19:35 +1100

    added the routes to the app and new components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './assets/Pages/auth/Login'
+import AdminSignup from './assets/Pages/auth/AdminSignup'
+import ResetPassword from './assets/Pages/auth/ResetPassword'
+import Dashboard from './assets/Pages/Dashboard/Dashboard'
+import AddBook from './assets/Pages/Books/AddBook'
+import EditBook from './assets/Pages/Books/EditBook'
+import Books from './assets/Pages/Books/Books'
+
 
 function App() {
  
 
   return (
     <>
-      <div>
-        </div>
+     <Routes>
+      <Route path='/' element={ <Login />} />
+      <Route path='/login' element={ <Login />} />
+      <Route path='/admin-signup' element={ <AdminSignup />} />
+      <Route path='/reset-password' element={ <ResetPassword />} />
+
+      <Route path='/dashboard' element={<Dashboard/>} />
+      
+      <Route path='/history' element={<History/>} />
+      
+      <Route path='/books/add' element={<AddBook/>} />
+      <Route path='/books/edit' element={<EditBook/>} />
+      <Route path='/books' element={<Books/>} />
+
+
+    
+     </Routes>
     </>
   )
 }
