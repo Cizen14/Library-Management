94ceb88a4a8443fe090ec7a329e5ba1f3aa28547
Cizen14 <cizenmah@gmail.com>
2024-01-16 19:01:46 +1100

    pages added and installed with router

---
+++ src/assets/Pages/Books/AddBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddBook = () => {
+  return (
+    <div>
+      addbook
+    </div>
+  )
+}
+
+export default AddBook
---
+++ src/assets/Pages/History/History.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <>
+      History Component
+    </>
+  )
+}
+
+export default History
