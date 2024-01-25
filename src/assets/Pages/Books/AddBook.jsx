4ce223e2e8788f4bfb3def55ce4ec468e31e61e3
Cizen14 <cizenmah@gmail.com>
2024-01-25 20:04:50 +1100

    added the new book page and done with the text

--- src/assets/Pages/Books/AddBook.jsx
+++ src/assets/Pages/Books/AddBook.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminLayout from '../../../Components/Layout/AdminLayout'
+import { Button, Form } from 'react-bootstrap'
+import CustomInput from '../../../Components/CustomInput/CustomInput'
+
+const inputs = [
+ 
+  { name: "isbn",label:"ISBN", placeholder: "ISBN", type:"text", required:true},
+  { name: "title",label:"Book Title", placeholder: "Enter Book Title", type:"text", required:true},
+  { name: "author",label:"Author Name", placeholder: "Enter Author Name", type:"text", required:true},
+  { name: "summary",label:"Summary", placeholder: "Enter Summary", as :"textarea", required:true, rows:4},
+  { name: "Year",label:"Published Year", placeholder: "2000", type:"number", required:true},
+  { name: "url",label:"Image Url", placeholder: "https://image-url.com", required:true},
+ 
+]
 
 const AddBook = () => {
+  const [formData, setFormData] = useState({});
+  
+  const handleChange = (e)=>{
+    const {name , value} = e.target;
+    setFormData({...formData, [name]: value});
+  }
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    const {email, password} = formData;
+    try{
+      
+     }
+
+  catch(e) {
+   
+}
+  }
+
   return (
     <div>
      <AdminLayout title={"Add Book"}>
+     <div className='p-3 border shadow rounded login-form'>
+    <h1>New Book Info</h1>
+      <Form onSubmit={handleSubmit}>
+      {
+        inputs.map(input=>(
+          <CustomInput key={input.name} label={input.label} onChange={handleChange}{...input}/>
+        ))
+      }
+     
+      <Button variant="primary" type="submit">
+        Add Book
+      </Button>
+    </Form>
+    </div>
           
-          </AdminLayout>
+     </AdminLayout>
     </div>
   )
 }
--- src/assets/Pages/Books/Books.jsx
+++ src/assets/Pages/Books/Books.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import AdminLayout from '../../../Components/Layout/AdminLayout'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 const Books = () => {
   return (
    
     <AdminLayout title={"Books"}>
+      <Link to={'/books/add'} className='d-flex justify-content-end'>
+      <Button>Add a Book</Button>
+      </Link>
+      {/* book list in a table */}
           
-          </AdminLayout>
+    </AdminLayout>
    
   )
 }
