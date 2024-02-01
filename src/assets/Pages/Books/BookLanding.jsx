e541cd297acdbffe0e51718eb5f83bcd6be7d8b3
Cizen14 <cizenmah@gmail.com>
2024-02-01 19:05:58 +1100

    booklanding page display done

--- src/assets/Pages/Books/BookLanding.jsx
+++ src/assets/Pages/Books/BookLanding.jsx
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import BaseLayout from "../../../Components/BaseLayout";
+import { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
 
 const BookLanding = () => {
   const { id } = useParams();
+  const {userInfo} = useSelector(state=>state.auth)
   console.log(id)
+  const {bookList} = useSelector(state => state.book);
+
+  const [ selectedBook, setSelectedBook] = useState({});
+  const navigate = useNavigate();
+
 
   // 1. Display the book detail based on the id
   // fetch book from redux
@@ -17,9 +27,45 @@ const BookLanding = () => {
 
   // login -> if path exists navigate to path else dashboard
 
+  useEffect(()=>{
+      const currentBook = bookList.find(book => book.id=== id);
+      if(currentBook){
+        setSelectedBook(currentBook);
+      }
+      else{
+          navigate('/');
+      }
+      
+      
+  }, [])
+
   return (
     <BaseLayout>
-      Individual Book
+      <Container>
+        <Row>
+          <Link to={'/'}><Button> Go Back</Button> </Link>
+
+        </Row>
+        <Row>
+          <Col>
+            <img src={selectedBook.url} alt="book image"/>
+          </Col>
+          <Col>
+            <h3>{selectedBook.title}</h3>
+            <p> By {selectedBook.author}</p>
+            <p>Ratin 5 star</p>
+            <p>Published {selectedBook.year}</p>
+            <p>{selectedBook.summary}</p>
+            <div>
+              {userInfo.uid ? (<Button>Burrow</Button>) :(<Link to={'/login'} state= {{ path:`books/${id}`}}><Button>Login to Burrow</Button></Link>)}
+              
+            </div>
+
+          </Col>
+          
+        </Row>
+      </Container>
+
     </BaseLayout>
   )
 }
